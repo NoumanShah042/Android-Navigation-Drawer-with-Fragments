4c52d3bd43dfce5128d72c315c6c2073e65b5287
NoumanShah042 <noumanrehman042@gmail.com>
2022-01-13 02:02:02 +0500

    adding fragments

--- app/src/main/java/com/example/navigation_drawer_with_fragments/MainActivity.java
+++ app/src/main/java/com/example/navigation_drawer_with_fragments/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.navigation_drawer_with_fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -9,15 +13,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -30,20 +29,62 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);  //  we can modify it for menu options if we create one
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);  //  to attach fragments
+        navigationView.setNavigationItemSelectedListener(this);
 
         //  this will create HAMBURGER ICON in toolbar ( to open close the drawer and also add animation )
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        // if we rotate our device,  the activity is destroyed and recreated.
+        // but normally the navigation drawer will keep the selected item even if we rotate the device ,
+        //    it will still show the correct fragment.
+        // but it should not override the fragment  ,  to Avoid it we can check as given below:
+
+        if (savedInstanceState == null) {   // when activity is created for the first time, savedInstanceState is null
+            // when we start our activity we will start our MessageFragment immediately before we click any item
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MessageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_message);
+        }
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed() {   //  on backPress if drawer is open close it , otherwise close the activity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ChatFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START); //  close draw on left side
+        return true;
     }
 }
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Navigation Drawer with Fragments
+
+
+### Developed By [Syed Nouman](https://github.com/NoumanShah042)
+ 
+## Description
+
+Navigation Drawer with the menu items open the fragments
+                                 
+
+## Screenshots
+
+* Add Activity Screen.
+<img src="screenshots/a.png" width="300">
+
+* Todo List View.
+<img src="screenshots/b.png" width="300">
